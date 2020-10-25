2e3c4617c27849efb7dc7af17bb9100afa7d776d
Tony Minseok Kim <tonyminseokkim11@gmail.com>
2020-10-24 20:54:34 -0700

    adding View into answerButton component

--- components/quizListDemo/answerButton.js
+++ components/quizListDemo/answerButton.js
@@ -5,17 +5,21 @@ import { RadioButton, Text } from 'react-native-paper';
 const answerButton = () => {
   const [value, setValue] = React.useState('first');
 
+  
+
   return (
-    <RadioButton.Group onValueChange={value => setValue(value)} value={value}>
-      <View>
-        <Text>True</Text>
-        <RadioButton value="first" />
-      </View>
-      <View>
-        <Text>False</Text>
-        <RadioButton value="second" />
-      </View>
-    </RadioButton.Group>
+    <View>
+      <RadioButton.Group onValueChange={value => setValue(value)} value={value}>
+        <View>
+          <Text>True</Text>
+          <RadioButton value="first" />
+        </View>
+        <View>
+          <Text>False</Text>
+          <RadioButton value="second" />
+        </View>
+      </RadioButton.Group>
+    </View>
   );
 };
 
